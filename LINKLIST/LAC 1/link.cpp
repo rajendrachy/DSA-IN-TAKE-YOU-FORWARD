411d75172c9e2194294f97c8873d267c4f27f721
rajendrachy <chyrajendra32@gmail.com>
2025-02-01 16:09:53 +0530

    add

---
+++ LINKLIST/LAC 1/link.cpp
@@ -0,0 +1,14 @@
+//___________________Linked List________________________
+//--------------Array--------------------------
+// int arr[4] = [1, 3, 2, 5] 
+// Array is of fixed size. Array is store in contegious location.
+// We cannot increase the size of array and we can not decrease the size of array
+
+
+
+// ------------------Linked_List--------------------------
+// Linked list -> Linked list help you to increase the size and decrease the size
+// of array.
+// Linked list is the similar data structure but not in the 
+// contegious location.
+
