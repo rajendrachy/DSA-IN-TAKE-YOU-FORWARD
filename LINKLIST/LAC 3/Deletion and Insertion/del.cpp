a1d1585b04b930445e0f50b165739330fa186307
rajendrachy <chyrajendra32@gmail.com>
2025-02-03 22:34:34 +0530

    add

---
+++ LINKLIST/LAC 3/Deletion and Insertion/del.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    
+    Node(int data) {
+        this->data = data;
+        next = nullptr;
+    }
+};
+
+Node* convertArr2LL(const vector<int>& arr) {
+    if (arr.empty()) return nullptr;
+    
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    
+    for (int i = 1; i < arr.size(); ++i) {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    
+    return head;
+}
+
+void print(Node* head) {
+    while (head != nullptr) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+Node* removesHead(Node* head) {
+    if (head == nullptr) return head;
+    
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    
+    return head;
+}
+
+int main() {
+    vector<int> arr = {12, 5, 8, 7};
+    Node* head = convertArr2LL(arr);
+    head = removesHead(head);
+    print(head);
+    
+    return 0;
+}
+
+
