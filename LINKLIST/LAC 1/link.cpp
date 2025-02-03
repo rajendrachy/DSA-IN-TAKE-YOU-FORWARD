4c24f722a1fac3de69091b932afab2faa98a435c
rajendrachy <chyrajendra32@gmail.com>
2025-02-03 13:46:32 +0530

    add

--- LINKLIST/LAC 1/link.cpp
+++ LINKLIST/LAC 1/link.cpp
@@ -5,10 +5,16 @@
 // We cannot increase the size of array and we can not decrease the size of array
 
 
-
 // ------------------Linked_List--------------------------
 // Linked list -> Linked list help you to increase the size and decrease the size
 // of array.
 // Linked list is the similar data structure but not in the 
 // contegious location.
+// The size can be increase at any movement.
+// We can increase the size and decrease the size in the linked List.
+
+
+//------------------
+// The linked list is store in the HEAP.
+// Head -> beginning of the LINKED-LIST.
 
---
+++ LINKLIST/LAC 2/link.cpp
@@ -0,0 +1,77 @@
+// #include<iostream>
+// #include<bits/stdc++.h>
+// #include<vector>
+// using namespace std;
+
+
+// class Node {
+//      public:
+//      int data; // data
+//      Node* next; // Store pointer to the next node
+
+//      public:
+//      Node(int data1, Node* next1) {
+//         data = data1;
+//         next = next1;
+//      }
+// };
+
+// int main() {
+//     vector<int> arr = {2, 5, 8, 7};
+//     Node* y = new Node(arr[0], nullptr); // new Keyworg give directly to the pointer to the memory location and we can store it in the variable -> y 
+//     cout << y->data; // 2
+//     return 0;
+// }
+
+
+
+
+// --------------Convert Array to the LInked List------------------------------
+// int arr[] = [2,1,3,8];
+// 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* convertArr2LL(vector<int> &arr) {
+    if (arr.empty()) return nullptr;
+
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i = 1; i < arr.size(); i++) {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+int main() {
+    vector<int> arr = {2, 5, 8, 7};
+    Node* head = convertArr2LL(arr);
+    
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+
+    return 0;
+}
+
+
+
+// Searching in the array -> Leetcode, GeeksforGeeks
